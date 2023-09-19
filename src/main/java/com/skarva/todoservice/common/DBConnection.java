85f9283210b46c6c72b403e4f850408b0e334408
reaswar <reaswar@penguin>
2023-09-19 11:51:57 +0530

    move password to props file

--- src/main/java/com/skarva/todoservice/common/DBConnection.java
+++ src/main/java/com/skarva/todoservice/common/DBConnection.java
@@ -2,6 +2,7 @@ package com.skarva.todoservice.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
@@ -10,13 +11,17 @@ import java.sql.DriverManager;
 @Component
 public class DBConnection {
 
+    // TODO: make this into a connection pool and extract out as config class 
+    @Value("${db.password}")
+    String pwd;
     Logger LOG = LoggerFactory.getLogger(DBConnection.class);
     public Connection getConnection() {
         Connection connection = null;
+
         try {
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/skarva",
-                    "skarva", "12345");
-            LOG.info("weeeeeeeeeeeeeeeeeeeeeeeeeeeeee db call!!!! {}", connection.getClientInfo());
+                    "skarva", pwd);
+            LOG.info("db call!!!! {}", connection.getClientInfo());
             return connection;
         } catch (Exception e) {
             throw new RuntimeException("Cannot connect to db!!:" + e);
