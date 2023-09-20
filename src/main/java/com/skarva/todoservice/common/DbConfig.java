f11af53ef8cd29a37505f5e250636c516bf72d3c
Ramanan Easwar <ramanan@Ramanans-MacBook-Air.local>
2023-09-20 19:40:03 +0530

    fix build failures

--- src/main/java/com/skarva/todoservice/common/DbConfig.java
+++ src/main/java/com/skarva/todoservice/common/DbConfig.java
@@ -3,9 +3,10 @@ package com.skarva.todoservice.common;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @Getter
 @Setter
 public class DbConfig {
